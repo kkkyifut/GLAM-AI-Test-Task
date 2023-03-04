102b54e13194589dff979d4b96c12624ffa02d22
Yuriy <ura.yakovlev@yandex.ru>
2023-03-04 21:40:25 +0500

    Initial Commit

---
+++ GLAM AI Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLAM AI Test Task
+//
+//  Created by Юрий Яковлев on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
